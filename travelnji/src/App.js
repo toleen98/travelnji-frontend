89e7f5e0f1b55eb5b9a613f22341605612348d09
nosibah93 <hefa.elayan@gmail.com>
2020-09-29 14:34:34 +0300

    contact component done

--- travelnji/src/App.js
+++ travelnji/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Map from "./components/map"
-
+import StickyFooter from "./components/contact"
 function App() {
   return (
     <div className="App">
       <Map/>
+      <StickyFooter/>
     </div>
   );
 }
