631abae46b907c4712ce5caf07424152c209a313
oriebalzyuot96 <alzuotorieb9999@gmail.com>
2020-09-29 20:55:03 +0300

    shop done 80%

--- travelnji/src/App.js
+++ travelnji/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+// import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
       
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react'
+
+
+
+import Shop from './component/Shop'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// import CheckoutForm from "./checkOutForm";
+
+// import AboutOur from "./about";
+
+
+class App extends Component {
+  render() {
+    return (
+      
+    <div className="App">
+      <Shop/>
     </div>
-  );
+
+    )
+  }
 }
 
-export default App;
+export default App
